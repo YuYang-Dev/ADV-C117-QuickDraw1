81a941d66c343ec5dfa5d30563b1c747c7d3fcbb
YuYang-Dev <79308509+YuYang-Dev@users.noreply.github.com>
2022-01-21 18:24:48 +0800

    Add files via upload

---
+++ potassium.js
@@ -0,0 +1,13 @@
+// wow i am so hard working did not copy this from any source
+
+function setup()
+{
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+}
+
+function clearCanvas()
+{
+    background("white");
+}
